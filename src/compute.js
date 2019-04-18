a3a7f8409a4dfae09f698181acc9182a14a08faf
dpp <dpp@dppdeMacBook-Pro.local>
2019-04-19 01:27:36 +0800

    v1.0.21

--- src/compute.js
+++ src/compute.js
@@ -17,38 +17,70 @@ const compute = {
 	},
 	/* 减运算，避免数据相除小数点后产生多位数和计算精度损失。*/
 	subtractNumber(...values) {
-		let difference = 0; // 差
-		let valuesLength = values.length;
-		for( let i = 0, len = valuesLength; i < len; i++ ){
-            difference -= (values[i] * 1000);
-        }
-		return difference / 1000; //防止丢失精度
+		if(values instanceof Array){
+			let difference = 0; // 差
+			let valuesLength = values.length;
+			for( let i = 0, len = valuesLength; i < len; i++ ){
+	            difference -= (values[i] * 1000);
+	        }
+			return difference / 1000; //防止丢失精度
+		}else{
+			return {
+				message: '传入的数据类型错误'
+			};
+		}
 	},
 	/* 乘运算，避免数据相除小数点后产生多位数和计算精度损失。*/
 	subNumber(...values) {
-		let product = 0; // 积
-		let valuesLength = values.length;
-		for( let i = 0, len = valuesLength; i < len; i++ ){
-            product *= (values[i] * 1000);
-        }
-		return product / (1000*valuesLength); //防止丢失精度
+		if(values instanceof Array){
+			let product = 0; // 积
+			let valuesLength = values.length;
+			for( let i = 0, len = valuesLength; i < len; i++ ){
+	            product *= (values[i] * 1000);
+	        }
+			return product / (1000*valuesLength); //防止丢失精度
+		}else{
+			return {
+				message: '传入的数据类型错误'
+			};
+		}
 	},
 	/* 除运算，避免数据相除小数点后产生多位数和计算精度损失。*/
 	divideNumber(...values) {
-		let quotient = 0; // 商
-		let valuesLength = values.length;
-		for( let i = 0, len = valuesLength; i < len; i++ ){
-            quotient /= (values[i] * 1000);
-        }
-		return quotient; //防止丢失精度
+		if(values instanceof Array){
+			let quotient = 0; // 商
+			let valuesLength = values.length;
+			for( let i = 0, len = valuesLength; i < len; i++ ){
+	            quotient /= (values[i] * 1000);
+	        }
+			return quotient; //防止丢失精度
+		}else{
+			return {
+				message: '传入的数据类型错误'
+			};
+		}
 	},
 	// 把值转换成整数
 	parseInt(value) {
-		return parseInt(value)
+		let resultType = typeof value;
+		if(resultType == 'number'){
+			return parseInt(value);
+		}else{
+			return {
+				message: '传入的数据类型错误'
+			};
+		}
 	},
 	// 把值转换成浮点数
 	parseFloat(value) {
-		return parseInt(value)
+		let resultType = typeof value;
+		if(resultType == 'number'){
+			return parseInt(value)
+		}else{
+			return {
+				message: '传入的数据类型错误'
+			};
+		}
 	},
 	// 强制类型转换-把给定的值转换成Boolean型
 	Boolean(value) {
@@ -64,31 +96,79 @@ const compute = {
 	},
 	// 向上取整
 	ceil(value) {
-		return Math.ceil(value);
+		let resultType = typeof value;
+		if(resultType == 'number'){
+			return Math.ceil(value);
+		}else{
+			return {
+				message: '传入的数据类型错误'
+			};
+		}
 	},
 	// 向下取整
 	floor(value) {
-		return Math.floor(value);
+		let resultType = typeof value;
+		if(resultType == 'number'){
+			return Math.floor(value);
+		}else{
+			return {
+				message: '传入的数据类型错误'
+			};
+		}
 	},
 	// 四舍五入
 	round(value) {
-		return Math.round(value);
+		let resultType = typeof value;
+		if(resultType == 'number'){
+			return Math.round(value);
+		}else{
+			return {
+				message: '传入的数据类型错误'
+			};
+		}
 	},
 	// 返回给定的数组中的较大的数
 	max(array) {
-		return Math.max.apply(null,array);
+		if(array instanceof Array){
+			return Math.max.apply(null,array);
+		}else{
+			return {
+				message: '传入的数据类型错误'
+			};
+		}
 	},
 	// 返回给定的数组中的较小的数
 	min(array) {
-		return Math.min.apply(null,array);
+		if(array instanceof Array){
+			return Math.min.apply(null,array);
+		}else{
+			return {
+				message: '传入的数据类型错误'
+			};
+		}
 	},
 	// 四舍五入,保留几位小数
 	toFixed(value) {
-		return Math.toFixed(value);
+		let resultType = typeof value;
+		if(resultType == 'number'){
+			return Math.toFixed(value);
+		}else{
+			return {
+				message: '传入的数据类型错误'
+			};
+		}
 	},
 	// 不四舍五入,保留几位小数
 	decimal(value, n) {
-		return Math.floor(value * 10 * n) / (100 * n)
+		let resultTypeValue = typeof value;
+		let resultTypeNum = typeof n;
+		if(resultType=='number' && resultTypeNum=='number'){
+			return Math.floor(value * 10 * n) / (100 * n)
+		}else{
+			return {
+				message: '传入的数据类型错误'
+			};
+		}
 	}
 }
 
